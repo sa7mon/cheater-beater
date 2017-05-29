c746f6d27455f7744be1b739cb74dfb111935527
Dan Salmon <salmon@protonmail.ch>
2017-05-29 22:19:56 +0200

    Add args and add process check

--- beater.py
+++ beater.py
@@ -2,6 +2,9 @@
 
 import json
 import hashlib
+import psutil
+import argparse
+
 
 workingDirectory = "./good-game/"
 
@@ -9,7 +12,7 @@ def enforceFileRule(fileName, fileHash):
 	fileName = workingDirectory + fileName
 	print("	File name: " + str(fileName) + " File hash: " 
 			+ str(fileHash))
-	
+
 	# Open file and calculate MD5 hash
 	with open(fileName) as file_to_check:
 	    md5_returned = hashlib.md5(file_to_check.read()).hexdigest()
@@ -21,11 +24,36 @@ def enforceFileRule(fileName, fileHash):
 
 
 def enforceProcessNameRule(procName):
-	print("	Enforced process rule for process: " + procName)
+	# print("	Enforced process rule for process: " + procName)
+	foundBadProc = False
+
+	for proc in psutil.process_iter():
+	    try:
+	        pinfo = proc.as_dict(attrs=['pid', 'name'])
+	    except psutil.NoSuchProcess:
+	        pass
+	    else:
+	        # print(pinfo)
+	        if pinfo.get("name") == "procName":
+			foundBadProc = True
+
+	print("Bad proccess running!")
 
 
 def enforceMemoryStringRule(memString):
 	print("	Enforced memory rule for string: " + memString)
+
+
+# Instantiate the parser
+parser = argparse.ArgumentParser(description='Fight those cheaters!')
+
+# Add directory arg
+parser.add_argument('gameDirectory', help='Directory where game is stored')
+
+# Parse the args
+args = parser.parse_args()
+
+workingDirectory = args.gameDirectory
 
 
 with open('config.json') as f:
